31415add027bbcad3cba38e66c16131f4c47e4c1
Caio Macario - Desenvolvedor Web <caiomacario016@gmail.com>
2026-05-17 23:58:20 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const conectarBanco = require("./config/banco");
+const conectarBanco = require("./banco");
 const app = express();
 
 conectarBanco();
