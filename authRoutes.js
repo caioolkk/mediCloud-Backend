c2d45f615150e1484e7a7d17c388ac450863163c
Caio Macario - Desenvolvedor Web <caiomacario016@gmail.com>
2026-05-23 23:08:07 -0300

    Delete authRoutes.js

--- authRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const authController = require("./authController");
-
-router.post("/registrar", authController.registrar);
-
-router.post("/login", authController.login);
-
-module.exports = router;
